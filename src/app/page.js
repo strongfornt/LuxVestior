4e33c001c03f4bb43dfec03f0ddb0c9a99de8f6d
storngfornt <au0038173@gmail.com>
2024-08-31 12:59:29 +0600

    post development added

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <>
       <h1>hello</h1>
-      <Slider/>
-      <h2>
-        hi
-      </h2>
+     
     </>
   );
 }
--- src/components/HomeComponents/Slider/Slider.jsx
+++ src/components/HomeComponents/Slider/Slider.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Slider() {
   return (
+    <>
     <div>Slider</div>
+    <h1>post development</h1>
+    </>
   )
 }
