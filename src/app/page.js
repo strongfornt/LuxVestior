7ce2d8f62f5bd5b9b3490367bae7648baa1cb109
storngfornt <au0038173@gmail.com>
2024-08-31 11:37:02 +0600

    slider component added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Slider from "@/components/HomeComponents/Slider/Slider";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h1>hello</h1>
+      <Slider/>
     </>
   );
 }
